b5d5edc611a7549adb07ae0a9d9c667ef420058e
mounikabalu95 <mounikabalu95@gmail.com>
2025-04-01 14:22:43 +0530

    Changes made to the folder structure

---
+++ src/test/java/runners/Testrunner.java
@@ -0,0 +1,17 @@
+package runners;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources",
+        glue = {"stepDefinitions", "hooks"},
+        plugin = {"pretty", 
+              "html:target/cucumber-reports.html",
+              "json:target/cucumber-reports.json",
+              "junit:target/cucumber-reports.xml"}
+)
+public class Testrunner {
+}
